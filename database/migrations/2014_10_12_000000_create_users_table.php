da3bc72fdc1a43cf2ee2c1ab15183bfaaf50ca0b
Mesa <mesa.sout@bi-kay.com>
2016-12-01 15:05:04 +0700

    added migration files

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,9 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->increments('id')->primary();
+            $table->string('name', 50);
+            $table->string('email',100)->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
---
+++ database/migrations/2016_12_01_071609_create_staffs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStaffsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staffs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->string('gender', 10);
+            $table->string('email', 50)->unique();
+            $table->string('phone', 15);
+            $table->date('date_of_birth');
+            $table->text('place_of_birth');
+            $table->string('mother_name', 50);
+            $table->string('father_name', 50);
+            $table->string('profile');
+            $table->text('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('staffs');
+    }
+}
